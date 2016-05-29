0470a42b1dc7d0d03caaa300a32d7b1f08e51052
David Tiago Conceição <davidtc.developer@gmail.com>
2016-05-29 15:28:29 -0300

    Adjusting gson adapter factory to better use reflection.

--- app/src/main/java/com/davidtcdeveloper/autovaluestudy/AutoValueTypeAdapterFactory.java
+++ app/src/main/java/com/davidtcdeveloper/autovaluestudy/AutoValueTypeAdapterFactory.java
@@ -13,8 +13,7 @@ public class AutoValueTypeAdapterFactory implements TypeAdapterFactory {
     @Override
     public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
         Class<? super T> rawType = type.getRawType();
-        if (rawType.equals(PersonData.class)
-                || rawType.equals(AutoValue_PersonData.class)) {
+        if (PersonData.class.isAssignableFrom(rawType)) {
             return (TypeAdapter<T>) PersonData.typeAdapter(gson);
         }
         return null;
